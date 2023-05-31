4a045c9f080c4caf936e67670bf892da4cbf092b
ZeroMinus13 <zerominus13@gmail.com>
2023-05-31 19:05:06 +0500

    testing railway

--- app.ts
+++ app.ts
@@ -1,37 +1,37 @@
-import express, { Request, Response, NextFunction } from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import { routes } from './routes/routes'
-import passport from './passportconfig'
-import mongoose from 'mongoose'
-import z from 'zod'
-dotenv.config()
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { routes } from './routes/routes';
+import passport from './passportconfig';
+import mongoose from 'mongoose';
+import z from 'zod';
+dotenv.config();
 
-const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 mongoose
   .connect(MONGODB_URI || 'mongodb://localhost:1000/mydb')
   .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Failed to connect to MongoDB', err))
+  .catch((err) => console.error('Failed to connect to MongoDB', err));
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(passport.initialize())
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
-app.use(routes)
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(routes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err)
+  console.error(err);
   if (err.name === 'MongoServerError') {
-    res.status(400).json({ error: 'Username already exists' })
+    res.status(400).json({ error: 'Username already exists' });
   } else if (err instanceof z.ZodError) {
-    console.log(err)
-    res.status(400).json({ error: err.errors[0].message })
+    console.log(err);
+    res.status(400).json({ error: err.errors[0].message });
   } else {
-    res.status(500).json({ error: 'Something broke' })
+    res.status(500).json({ error: 'Something broke' });
   }
-})
+});
 
-export default app
+export default app;
--- routes/routes.ts
+++ routes/routes.ts
@@ -1,25 +1,25 @@
-import { Router } from 'express'
-import passport from '../passportconfig'
-import { getBlogData, createBlog, deleteBlog, updateBlog, getSingleBlog } from '../controller/blogdata'
-import { createComments, deleteComments } from '../controller/comments'
-import { createAdmin, logAdmin } from '../controller/blogAdmin'
+import { Router } from 'express';
+import passport from '../passportconfig';
+import { getBlogData, createBlog, deleteBlog, updateBlog, getSingleBlog } from '../controller/blogdata';
+import { createComments, deleteComments } from '../controller/comments';
+import { createAdmin, logAdmin } from '../controller/blogAdmin';
 
-const router = Router()
-const protectedRoute = passport.authenticate('jwt', { session: false })
+const router = Router();
+const protectedRoute = passport.authenticate('jwt', { session: false });
 
-router.get('/p', protectedRoute, (req, res) => res.json({ message: 'bing Bing' }))
-router.get('/', getBlogData)
-router.post('/', protectedRoute, createBlog)
-router.put('/:id/updateBlog', protectedRoute, updateBlog)
+router.get('/p', protectedRoute, (req, res) => res.json({ message: 'bing Bing' }));
+router.get('/', getBlogData);
+router.post('/', protectedRoute, createBlog);
+router.put('/:id/updateBlog', protectedRoute, updateBlog);
 
-router.get('/:id', getSingleBlog)
-router.delete('/:id/deleteblog', protectedRoute, deleteBlog)
+router.get('/:id', getSingleBlog);
+router.delete('/:id/deleteblog', protectedRoute, deleteBlog);
 
-router.post('/comments/:id/create', createComments)
+router.post('/comments/:id/create', createComments);
 
-router.delete('/comments/:id/delete', protectedRoute, deleteComments)
+router.delete('/comments/:id/delete', protectedRoute, deleteComments);
 
-router.post('/createAdmin', createAdmin)
-router.post('/login', logAdmin)
+router.post('/createAdmin', createAdmin);
+router.post('/login', logAdmin);
 
-export const routes = router
+export const routes = router;
